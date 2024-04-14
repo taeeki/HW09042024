5b4bf3d2ed47cd87bd3f01af04ad6a91c9670957
taeekii <terenteva1999@yandex.ru>
2024-04-14 13:24:58 +0300

    hw1

--- HW09042024/Program.cs
+++ HW09042024/Program.cs
@@ -1,2 +1,60 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System.Runtime.CompilerServices;
+
+Console.WriteLine("Информация о первом объекте");
+Phone phone1 = new Phone();
+
+Console.WriteLine("Информация о втором объекте");
+Phone phone2 = new Phone("+7888545693", "Samsung");
+
+Console.WriteLine("Информация о третьем объекте");
+Phone phone3 = new Phone("+78856941236", "samsung", 500.0);
+
+
+phone1.receiveCall("Иван");
+phone2.receiveCall("Юрий", phone2.getNumber());
+
+phone3.sendMessage(phone3.getNumber(), phone2.getNumber());
+class Phone
+{
+    string number;
+    string model;
+    double weight;
+
+    public Phone()
+    {
+        this.number = "+79859390444";
+        this.model = "iPhone";
+        this.weight = 200;
+        Console.WriteLine($"Номер телефона {number},\nМодель телефона {model},\nВес телефона: {weight}");
+    }
+    public Phone(string _number, string _model)
+    {
+        this.number = _number;
+        this.model = _model;
+        Console.WriteLine($"Номер телефона {number},\nМодель телефона {model}");
+    }
+    public string getNumber()
+    {
+        return number;
+    }
+    public Phone(string _num, string _mod, double _weight) : this(_num, _mod)
+    {
+        _weight = weight;
+        Console.WriteLine($"Вес телефона: {weight}");
+    }
+    public void receiveCall(string name)
+    {
+        Console.WriteLine($"Звонит {name}");
+    }
+
+
+    public void receiveCall(string name, string numberCall)
+    {
+        Console.WriteLine($"Звонит {name}, Номер телефона: {numberCall}");
+    }
+
+    public void sendMessage(string _phNumber1, string _phNumber2)
+    {
+        Console.WriteLine($"Сообщение будет отправлено на номера: {_phNumber1}, {_phNumber2} ");
+    }
+}
